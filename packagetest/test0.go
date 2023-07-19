4a783067a681d34807d70881f941190f30beb4bc
psakshaycode <76804806+psakshaycode@users.noreply.github.com>
2023-07-20 02:21:50 +0530

    add docstrings to packagetes

--- packagetest/test0.go
+++ packagetest/test0.go
@@ -1,8 +1,11 @@
+// Package packagetest intend to test the installation of riverutils
 package packagetest
 
 import "fmt"
 
 // SayHellTo says hello to the parameter
+//
+// Sample usage SayHelloTo("yourname")
 func SayHelloTo(name string) {
 	fmt.Printf("Hello %s\n", name)
 }
