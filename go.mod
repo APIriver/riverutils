4b08a4bf8cc1cb515b33c2957b0bc72a2265e68a
psakshaycode <76804806+psakshaycode@users.noreply.github.com>
2023-07-20 01:34:44 +0530

    add sayhello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/APIriver/riverutils
+
+go 1.20
---
+++ packagetest/test0.go
@@ -0,0 +1,8 @@
+package packagetest
+
+import "fmt"
+
+// SayHellTo says hello to the parameter
+func SayHelloTo(name string) {
+	fmt.Printf("Hello %s\n", name)
+}
